b41a36a9210539e69380346950f5c1908224b529
bibekkarkee <168980759+bibekkarkee@users.noreply.github.com>
2026-03-02 23:53:57 +0545

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -4,15 +4,13 @@ export default async function handler(req, res) {
   try {
     const response = await fetch(url);
     const text = await response.text();
-
-    // Parse text as JSON
     const data = JSON.parse(text);
 
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Content-Type", "application/json");
     res.status(200).json(data);
-  } catch (err) {
-    console.error(err);
+  } catch (error) {
+    console.error(error);
     res.status(500).json({ error: "Failed to fetch or parse JSON" });
   }
 }
