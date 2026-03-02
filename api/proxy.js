ea4078e8d45fdcbb206d6f0fccc4c367f7fd2745
bibekkarkee <168980759+bibekkarkee@users.noreply.github.com>
2026-03-03 01:04:47 +0545

    Refactor handler function formatting in proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,9 +1,9 @@
-export default async function handler(req, res) {
-  try {
-    const response = await fetch('https://result.election.gov.np/JSONFiles/ElectionResultCentral2082.txt');
-    const data = await response.json();
-    res.status(200).json(data);
-  } catch (err) {
-    res.status(500).json({ error: 'Failed to fetch data' });
-  }
+export default async function handler(req,res){
+    try{
+        const response = await fetch('https://result.election.gov.np/JSONFiles/ElectionResultCentral2082.txt');
+        const data = await response.json();
+        res.status(200).json(data);
+    }catch(err){
+        res.status(500).json({ error:'Failed to fetch data' });
+    }
 }
