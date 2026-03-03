5693ce4ed46e81a449aa58666af5710fabe285b2
bibekkarkee <168980759+bibekkarkee@users.noreply.github.com>
2026-03-03 18:43:16 +0545

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,9 +1,10 @@
-export default async function handler(req,res){
-    try{
-        const response = await fetch('https://result.election.gov.np/JSONFiles/ElectionResultCentral2082.txt');
-        const data = await response.json();
-        res.status(200).json(data);
-    }catch(err){
-        res.status(500).json({ error:'Failed to fetch data' });
-    }
+export default async function handler(req, res) {
+  const url = "https://result.election.gov.np/JSONFiles/ElectionResultCentral2082.txt";
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ error: "Cannot fetch data" });
+  }
 }
